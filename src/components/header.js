702b0b87d0a467dac55d98e419fafa96136ce9ce
Jeremy Smith <jeremy.smith@pantheon.io>
2020-05-08 22:23:58 -0700

    add routing and styles to desktop nav

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,47 @@
 import React from "react"
+import { Link }from 'gatsby';
 import styled from "@emotion/styled"
 import { css } from "@emotion/core"
-import { Link } from "gatsby"
 import Layout from "../components/layout.css"
 import Logo from "../images/playMarinLogo.png"
 
 const Header = () => {
-  const Link = styled.a`
-    padding-left: 10px;
+  const StyledLink = styled(Link)`
+    padding-left: 20px;
     cursor: pointer;
-  `
+     text-decoration: none;
+     color: #000;
+     margin-right: 20px;
+     border-bottom-width: 5px;
+     position: relatve;
+
+
+  `;
+
+
+const activeStyle = {
+  color: `#555`,
+  borderBottom: `2px solid #000`,
+}
 
   return (
     <>
       <nav className="navbar">
         <ul className="left-ui">
           <li>
-            <img width={100} height={42} src={Logo} />
+          <StyledLink to="/">
+            <img to="/" width={100} height={42} src={Logo} className="work" />
+            </StyledLink >
           </li>
-          <Link className="home">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/casestudy">PLAY Study</Link>
-          <Link to="/programs">Programs</Link>
-          <Link to="/gallery">Gallery</Link>
-          <Link to="/contact">Contact Us</Link>
+          <StyledLink to="/about/" activeStyle={activeStyle}>About</StyledLink>
+          <StyledLink to="/casestudy" activeStyle={activeStyle}>PLAY Study</StyledLink>
+          <StyledLink to="/programs/" activeStyle={activeStyle}>Programs</StyledLink>
+          <StyledLink to="/gallery/" activeStyle={activeStyle}>Gallery</StyledLink>
+          <StyledLink to="/contact/" activeStyle={activeStyle}>Contact Us</StyledLink>
         </ul>
         <ul className="right-ui">
-          <Link className="work">Login</Link>
-          <Link className="points">Donate</Link>
+          <StyledLink to="/" className="work">Login</StyledLink>
+          <StyledLink to="/" className="points">Sign up</StyledLink>
           <li>
             <i className="fas fa-bell bell"></i>
           </li>
---
+++ src/pages/contact.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Header from "../components/header.js"
+import Footer from "../components/footer.js"
+
+
+
+
+
+
+const Contact = () => {
+  return (
+    <>
+    <Header/>
+    This is the form
+    <Footer/>
+    </>
+  )
+}
+
+
+export default Contact
