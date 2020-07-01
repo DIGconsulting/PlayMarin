99cc71684c2519fc028b3f01a1d09adfadb35713
Jeremy Smith <jeremy.smith@pantheon.io>
2020-07-01 14:56:39 -0700

    WIP

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,11 +3,14 @@ import classes from "./hero.module.css"
 import PlayMarin from "../images/playMarin.mp4"
 import { Jumbotron, Button, Container, Row, Col } from "react-bootstrap"
 import styles from "./form.module.css"
+import { Player } from 'video-react';
+
 
 const Hero = () => {
   return (
     <div className={classes.Container}>
-      <video autoPlay="autoplay" loop="loop" muted className={classes.Video}>
+      <video autoPlay="autoplay" loop="loop" muted className={classes.Video} fluid={false} playsInline
+>
         <source src={PlayMarin} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
@@ -15,52 +18,28 @@ const Hero = () => {
       <div className={classes.Content}>
         <div className={classes.SubContent}>
           <div>
-            <div
-              style={{
-                fontWeight: "600",
-                paddingBottom: "50px",
-                paddingTop: "100px",
-              }}
-            >
+            <div>
               <center>
-                <p
-                  style={{
-                    width: "50%",
-                    fontFamily: "avenir",
-                    textAlign: "left",
-                    FontSize: "20px",
-                  }}
+                <p className={classes.Herotext}
                 >
                   With the goal of closing the activity gap in Marin City while
                   promoting diversity and inclusion throughout Marin County,
                   Play Marin started 8 years ago with one Marin City basketball
                   team of 9 players.
-                </p>
-                <p
-                  style={{
-                    width: "50%",
-                    fontFamily: "avenir",
-                    textAlign: "left",
-                  }}
-                >
-                  {" "}
+                                  <br/>
+                                  <br/>
                   Today, Play Marin reaches more than 300 kids annually
                   throughout Marin with our mission of diversity and inclusion
                   through Play.
                 </p>
+
               </center>
             </div>
             <Row  className={classes.Overlay}
             >
               <Col  xs={6} sm={6} md={6} lg={6}>
                 <div>
-                  <p
-                    style={{
-                      fontSize: "18px",
-                      fontWeight: "600",
-                      paddingRight: "50px",
-                      fontFamily: "avenir",
-                    }}
+                  <p className={classes.Overlaytext}
                   >
                     Your support helps Play Marin close the actiivty gap in
                     Marin City and provides a more diverse peer group for over
@@ -72,6 +51,8 @@ const Hero = () => {
                 <div style={{
                   display: "flex",
                   flexDirection: "column",
+                  alignItems: "center",
+
 
                 }}>
                 <h5>Select An Amount:</h5>
