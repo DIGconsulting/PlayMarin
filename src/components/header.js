561f09c89949ba52c8e3dc775d150a5e0a4bddcf
Jeremy Smith <jeremy.smith@pantheon.io>
2020-07-16 06:46:50 -0700

    WIP

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,29 @@
 import React from "react"
 import Navbar from "react-bootstrap/Navbar"
-import NavDropdown from "react-bootstrap/NavDropdown"
+import {Col,} from "react-bootstrap/Navbar"
 import FormControl from "react-bootstrap/FormControl"
-import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
 import Nav from "react-bootstrap/Nav"
 import Container from "react-bootstrap/Container"
 import Logo from "../images/playMarinLogo.png"
-import "./hero.module.css"
 import { Link } from "gatsby"
 import styled from "@emotion/styled"
+import "./hero.module.css"
 
 const Header = () => {
   const StyledLink = styled(Link)`
-    padding-left: 20px;
     cursor: pointer;
+    display: flex;
+    flex-wrap: wrap;
     text-decoration: none;
     color: #6512ae;
     margin-right: 20px;
     border-bottom-width: 5px;
-    position: relatve;
     font-family: avenir;
   `
   const ExternalLink = styled(Link)`
-    padding-left: 20px;
     cursor: pointer;
     text-decoration: none;
-    margin-right: 20px;
     border-bottom-width: 5px;
     position: relatve;
     font-family: avenir;
@@ -39,13 +36,14 @@ const Header = () => {
   }
 
   return (
+    <>
     <Navbar collapseOnSelect expand="lg" fixed="top">
       <Container
         style={{
           backgroundColor: "white",
         }}
       >
-        <Navbar.Brand href="#home">
+        <Navbar.Brand>
           <img
             to="/"
             width={100}
@@ -58,25 +56,23 @@ const Header = () => {
           />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse
-          id="responsive-navbar-nav"
-          style={{
-            paddingLeft: "20px",
-          }}
-        >
-          <Navbar className="mr-auto">
+        <Navbar.Collapse>
+          <Nav className="mr-auto">
             <StyledLink to="/">Home</StyledLink>
-            <StyledLink to="/about">About</StyledLink>
+            <StyledLink to="/about">About</StyledLink> 
             <StyledLink to="programs">Programs</StyledLink>
             <StyledLink to="support">Support</StyledLink>
             <StyledLink to="contact">Contact Us</StyledLink>
-          </Navbar>
+          </Nav>
           <Link to="/support">
-            <Button variant="outline-success">Donate</Button>
+          <Button variant="outline-success">Donate</Button>
           </Link>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    </>
+    
+   
   )
 }
 
