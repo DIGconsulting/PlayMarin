732ba86e466078fabddc64b10cc7d2965b64908c
Jeremy Smith <jeremy.smith@pantheon.io>
2020-07-17 16:40:51 -0700

    WIP

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,11 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-
-exports.replaceHydrateFunction = () => {
-    return (element, container, callback) => {
-      console.log("rendering!");
-      ReactDOM.render(element, container, callback);
-    };
-  };
-  
