c9ae51f9bfa059e1eda8f989ed318394b1ca7603
Jeremy Smith <jeremy.smith@pantheon.io>
2020-07-27 11:17:34 -0700

    WIP

--- src/components/hero.js
+++ src/components/hero.js
@@ -11,38 +11,51 @@ import Container from "react-bootstrap/Container"
 import Jumbotron from "react-bootstrap/Jumbotron"
 import "./hero.module.css"
 import VideoMobile from "../images/playMarin.gif"
-import Media from "react-media"
+import Media from 'react-media'
 
 function Hero() {
+  const { video } = useStaticQuery(graphql`
+    query {
+      video: file(relativePath: { eq: "playmarin_.compressed.mp4" }) {
+        videoH264 {
+          path
+        }
+      }
+    }
+  `)
+
   return (
     <>
       <div className={classes.Container}>
-        <img
-          src={VideoMobile}
-          style={{
-            height: "1000px",
-          }}
-        />
 
-        <Media
-          query="(max-width: 599px)"
-          render={() => (
-            <img
-              src={VideoMobile}
-              style={{
-                height: "600px",
-              }}
-            />
+
+      <Media query="(max-width: 599px)" render={() =>
+          (
+            <img src={VideoMobile} style={{
+              height: "1000px"
+            }}/>
           )}
         />
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className={classes.Video}
+          fluid={false}
+        >
+          <source src={video.videoH264.path} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
 
+    
         <div className={classes.Content}>
           <Container>
             <Jumbotron
               style={{
                 background: "rgba(7, 58, 99, 0.5)",
                 padding: "1rem 1rem",
-                marginBottom: "0px",
+                marginBottom: "0px"
               }}
             >
               <Row className="justify-content-md-center">
